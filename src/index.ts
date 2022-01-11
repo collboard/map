749ec5161d798dae6167c3eaafdd0575f7488f6f
Pavol Hejny <me@pavolhejny.com>
2022-01-11 13:46:56 +0100

    TODOs

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,8 @@ import './modules/map';
 import './modules/map-controls';
 import './modules/map-polygon-art';
 
-// TODO: !!! Make multimodule packs
-// TODO: !!! Geojson support module
+/**
+ * TODO: !!! Make multimodule packs
+ * TODO: !!! Geojson support module to import
+ * TODO: Export all (freehands) as geojsons
+ */
