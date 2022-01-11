628542bb50bf82fb2dca04e4434cc5e91d6b039d
Pavol Hejny <me@pavolhejny.com>
2022-01-11 11:49:34 +0100

    WGS84 coords

--- src/index.tsx
+++ src/index.tsx
@@ -24,17 +24,22 @@ declareModule({
 
         // TODO: If constants to UPPERCASE and config
 
+        const tilePixelSize = Vector.square(256);
+        const tileCount = new Vector(2, 2 /* TODO: Count based on screen size (with some system) and tileSize */);
+
         const mapZoom = 8;
-        const centerCoords = new Vector(133, 83 /* Reverse */);
-        const tileSize = Vector.square(256);
-        const tileCount = new Vector(12, 8);
+        const mapCenterWgs84 = new Vector(14.4378005 /* Longitude  */, 50.0755381 /* Latitude  */);
+
+        const mapCenterTileXy = wgs84ToTileXy(mapCenterWgs84, mapZoom); // new Vector(133, 83 /* Reverse */);
+
+        const registration = Registration.void();
 
         for (let y = 0; y < tileCount.y; y++) {
             for (let x = 0; x < tileCount.x; x++) {
                 const tileCoords = new Vector(x, y);
                 const tileArt = new ImageArt(
                     `https://tile-a.openstreetmap.fr/hot/${mapZoom}/${tileCoords
-                        .add(centerCoords)
+                        .add(mapCenterTileXy)
                         .toArray2D()
 
                         .join('/')}.png`,
@@ -42,17 +47,35 @@ declareModule({
                 );
 
                 tileArt.defaultZIndex = -1;
-                tileArt.setShift(tileCoords.subtract(tileCount.half()).multiply(tileSize));
+                tileArt.setShift(tileCoords.subtract(tileCount.half()).multiply(tilePixelSize));
 
-                virtualArtVersioningSystem.createPrimaryOperation().newArts(tileArt).persist();
+                registration.addSubdestroyable(
+                    virtualArtVersioningSystem.createPrimaryOperation().newArts(tileArt).persist(),
+                );
             }
         }
 
-        return Registration.join();
+        return registration;
     },
 });
+
+function wgs84ToTileXy(coordinatesWgs84: Vector, zoom: number) {
+    return new Vector(
+        Math.floor(((coordinatesWgs84.x + 180) / 360) * Math.pow(2, zoom)),
+        Math.floor(
+            ((1 -
+                Math.log(
+                    Math.tan((coordinatesWgs84.y * Math.PI) / 180) + 1 / Math.cos((coordinatesWgs84.y * Math.PI) / 180),
+                ) /
+                    Math.PI) /
+                2) *
+                Math.pow(2, zoom),
+        ),
+    );
+}
 
 /**
  * TODO: Should be here explicitelly installed `destroyable`  library
  * TODO: XYZT 2D forEach
+ * TODO: XYZT semantic coordinates (latitude, longitude) with conversion
  */
