2f2f90afe74bbde7fb490d96016034c0a6c07b7a
Pavol Hejny <me@pavolhejny.com>
2022-01-13 22:02:48 +0100

    Infinite map

--- src/modules/map.tsx
+++ src/modules/map.tsx
@@ -4,6 +4,7 @@ import { Vector } from 'xyzt';
 import helloWorldIcon from '../../assets/hello-world-icon.png';
 import { contributors, description, license, repository, version } from '../../package.json';
 import { mapCenterTileXyRound, mapCenterTileXyRoundRemainder, mapProvider, mapZoom, tilePixelSize } from '../config';
+import { observeByHeartbeat } from '../utils/observeByHeartbeat';
 
 declareModule({
     manifest: {
@@ -39,37 +40,50 @@ declareModule({
             'notificationSystem',
         );
 
-        const tileCount = new Vector(6, 4 /* TODO: Count based on screen size (appState.windowSize) and tileSize */);
+        // TODO: Count based on screen size (appState.windowSize) and tileSize + observe
+        const tileCount = new Vector(6, 4);
 
         const registration = Registration.void();
 
-        for (let y = 0; y < tileCount.y; y++) {
-            for (let x = 0; x < tileCount.x; x++) {
-                const tileCoords = new Vector(x, y);
-                const tileArt = new ImageArt(
-                    // TODO: Map server and type provider
-                    `${mapProvider.href}/${mapZoom}/${tileCoords
-                        .add(mapCenterTileXyRound)
-                        .subtract(tileCount.half())
-                        .toArray2D()
+        observeByHeartbeat({ getValue: () => appState.transform }).subscribe((transform) => {
+            for (let y = 0; y < tileCount.y; y++) {
+                for (let x = 0; x < tileCount.x; x++) {
+                    const mapCenterTileOffset = transform.translate
+                        .divide(tilePixelSize)
+                        .map(Math.floor /* TODO: Floor OR round? */);
 
-                        .join('/')}.png`,
-                    'Map tile',
-                );
+                    const tileCoords = new Vector(x, y);
+                    const tileArt = new ImageArt(
+                        // TODO: Map server and type provider
+                        `${mapProvider.href}/${mapZoom}/${tileCoords
+                            .add(mapCenterTileXyRound)
+                            .subtract(mapCenterTileOffset)
+                            .subtract(tileCount.half())
+                            .toArray2D()
 
-                tileArt.defaultZIndex = -1;
-                tileArt.setShift(
-                    tileCoords
-                        .subtract(tileCount.half())
-                        .subtract(mapCenterTileXyRoundRemainder)
-                        .multiply(tilePixelSize),
-                );
+                            .join('/')}.png`,
+                        'Map tile',
+                    );
 
-                registration.addSubdestroyable(
-                    virtualArtVersioningSystem.createPrimaryOperation().newArts(tileArt).persist(),
-                );
+                    // TODO: Cache tiles and do not rerender them
+                    // TODO: Free tiles from memory
+                    // TODO: Fillup the screen by tiles (translate+zoom)
+
+                    tileArt.defaultZIndex = -1;
+                    tileArt.setShift(
+                        tileCoords
+                            .subtract(tileCount.half())
+                            .subtract(mapCenterTileXyRoundRemainder)
+                            .subtract(mapCenterTileOffset)
+                            .multiply(tilePixelSize),
+                    );
+
+                    registration.addSubdestroyable(
+                        virtualArtVersioningSystem.createPrimaryOperation().newArts(tileArt).persist(),
+                    );
+                }
             }
-        }
+        });
 
         /*/
         observeByHeartbeat({ getValue: () => appState.transform }).subscribe(() => {
@@ -90,5 +104,5 @@ declareModule({
 });
 
 /**
- *
+ * TODO: TileXy -> Tile
  */
