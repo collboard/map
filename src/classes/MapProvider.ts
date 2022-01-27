d0104dbaff1d400a81b858f136d47f6d9ce782b8
Pavol Hejny <me@pavolhejny.com>
2022-01-27 18:35:14 +0100

    Loading from multiple sources

--- src/classes/MapProvider.ts
+++ src/classes/MapProvider.ts
@@ -1,17 +1,21 @@
-import { AbstractArt, ImageArt, string_url_image } from '@collboard/modules-sdk';
+import { AbstractArt, ImageArt, randomItem } from '@collboard/modules-sdk';
 import { MAP_BASE, TILE_SIZE } from '../config';
 import { TileAbsolute } from '../semantic/TileAbsolute';
 import { TileUnique } from '../semantic/TileUnique';
 import { Wgs84 } from '../semantic/Wgs84';
 
 export class MapProvider {
-    // configurabe
-    // export const mapProvider = new URL('https://tile-a.openstreetmap.fr/hot');
-    private service = new URL('https://tile-c.openstreetmap.fr/cyclosm');
+    private getServiceUrl(): URL {
+        return new URL(`https://tile-${randomItem('a', 'b', 'c')}.openstreetmap.fr/cyclosm`);
+    }
 
-    private getTileUrl(tile: TileUnique): string_url_image {
+    private getTileUrl(tile: TileUnique): URL {
         const { x, y, z } = tile;
-        return `${this.service.href}/${z}/${x}/${y}.png`;
+
+        const url = this.getServiceUrl();
+        url.pathname = `cyclosm/${z}/${x}/${y}.png`;
+
+        return url;
     }
 
     public createTileArt(tile: TileUnique): AbstractArt /* TODO: Maybe return AbstractArt[] OR TileArt */ {
@@ -26,7 +30,7 @@ export class MapProvider {
 
         const tileUrl = this.getTileUrl(tile);
 
-        const tileArt = new ImageArt(tileUrl, 'Map tile');
+        const tileArt = new ImageArt(tileUrl.href, 'Map tile');
 
         tileArt.defaultZIndex = -1;
         tileArt.size = TILE_SIZE.scale(Math.pow(2, MAP_BASE.z - tile.z));
--- src/config.ts
+++ src/config.ts
@@ -2,14 +2,8 @@ import { MapProvider } from './classes/MapProvider';
 import { Pixels } from './semantic/Pixels';
 import { Wgs84 } from './semantic/Wgs84';
 
-// TODO: !!! to UPPERCASE
-
 export const TILE_SIZE = Pixels.square(256 /* TODO: Make some seam-padding */);
-export const TILE_COUNT_PADDING = 3;
+export const TILE_COUNT_PADDING = 1.2;
 
 export const MAP_PROVIDER = new MapProvider(/* TODO: Configure */);
 export const MAP_BASE = new Wgs84({ longitude: 14.5, latitude: 50, zoom: 12 /* 17 */ });
-
-/**
- * TODO: !!! mapProvider cycle between tile-a tile-b tile-c
- */
