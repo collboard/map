6e75dd6b4cd73296a31f9fe8ddec49eb21fc3f57
Pavol Hejny <me@pavolhejny.com>
2022-02-09 21:32:26 +0100

    TODOs

--- src/classes/MapManager.ts
+++ src/classes/MapManager.ts
@@ -178,7 +178,9 @@ export class MapManager extends Destroyable implements IDestroyable {
 }
 
 /**
- * TODO: !!! Fillup the screen by tiles (translate+zoom)
+ * TODO: !!! Some little shifting on changing tiles in deep zoom level
+ * TODO: !!! UX and precision of drawing in all zoom levels
+ * TODO: !!! Remove small stripes
  * TODO: !!! Import MAP_BASE_CENTER only from one place
  * TODO: Some calculations are made inefficiently in a 2D loop instead of once before it
  */
