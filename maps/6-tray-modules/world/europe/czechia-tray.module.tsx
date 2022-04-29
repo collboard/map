403b14f59c8e47b812f1816cbd04717185a0f230
Pavol Hejny <me@pavolhejny.com>
2022-04-29 11:29:25 +0200

    🚡 Make tray module from svgs
    🔼 This commit was automatically generated by map scripts

--- maps/6-tray-modules/world/europe/czechia-tray.module.tsx
+++ maps/6-tray-modules/world/europe/czechia-tray.module.tsx
@@ -5,37 +5,36 @@
 
 import { declareModule, makeTraySimpleModule } from '@collboard/modules-sdk';
 import { contributors, license, repository, version } from '../../../../package.json';
-import hlavniMestoPrahaPrahaCesko from '../../../4-svgs/world/europe/czechia/czechia.aggregated4.geojson.lod-5.svg';
-import hlavniMestoPrahaPrahaCesko1 from '../../../4-svgs/world/europe/czechia/czechia.aggregated3.geojson.lod-5.svg';
-import hlavniMestoPrahaPrahaCesko2 from '../../../4-svgs/world/europe/czechia/czechia.aggregated2.geojson.lod-5.svg';
-import untitled from '../../../4-svgs/world/europe/czechia/czechia.geojson.lod-5.svg';
-import hlavniMestoPrahaPrahaCesko3 from '../../../4-svgs/world/europe/czechia/hlavni-mesto-praha/hlavni-mesto-praha.geojson.lod-5.svg';
-import jihomoravskyKraj from '../../../4-svgs/world/europe/czechia/jihomoravsky-kraj/jihomoravsky-kraj.geojson.lod-5.svg';
-import jihoceskyKraj from '../../../4-svgs/world/europe/czechia/jihocesky-kraj/jihocesky-kraj.geojson.lod-5.svg';
-import karlovarskyKraj from '../../../4-svgs/world/europe/czechia/karlovarsky-kraj/karlovarsky-kraj.geojson.lod-5.svg';
-import krajVysocinaJihovychodCesko from '../../../4-svgs/world/europe/czechia/kraj-vysocina/kraj-vysocina.geojson.lod-5.svg';
-import kralovehradeckyKraj from '../../../4-svgs/world/europe/czechia/kralovehradecky-kraj/kralovehradecky-kraj.geojson.lod-5.svg';
-import libereckyKraj from '../../../4-svgs/world/europe/czechia/liberecky-kraj/liberecky-kraj.geojson.lod-5.svg';
-import moravskoslezskyKraj from '../../../4-svgs/world/europe/czechia/moravskoslezsky-kraj/moravskoslezsky-kraj.geojson.lod-5.svg';
-import olomouckyKraj from '../../../4-svgs/world/europe/czechia/olomoucky-kraj/olomoucky-kraj.geojson.lod-5.svg';
-import pardubickyKraj from '../../../4-svgs/world/europe/czechia/pardubicky-kraj/pardubicky-kraj.geojson.lod-5.svg';
-import plzenskyKraj from '../../../4-svgs/world/europe/czechia/plzensky-kraj/plzensky-kraj.geojson.lod-5.svg';
-import stredoceskyKraj from '../../../4-svgs/world/europe/czechia/stredocesky-kraj/stredocesky-kraj.geojson.lod-5.svg';
-import zlinskyKraj from '../../../4-svgs/world/europe/czechia/zlinsky-kraj/zlinsky-kraj.geojson.lod-5.svg';
-import usteckyKraj from '../../../4-svgs/world/europe/czechia/ustecky-kraj/ustecky-kraj.geojson.lod-5.svg';
-import jihomoravskyKraj1 from '../../../4-svgs/world/europe/czechia/jihomoravsky-kraj/jihomoravsky-kraj.aggregated2.geojson.lod-5.svg';
-import jihoceskyKraj1 from '../../../4-svgs/world/europe/czechia/jihocesky-kraj/jihocesky-kraj.aggregated2.geojson.lod-5.svg';
-import karlovarskyKraj1 from '../../../4-svgs/world/europe/czechia/karlovarsky-kraj/karlovarsky-kraj.aggregated2.geojson.lod-5.svg';
-import krajVysocinaJihovychodCesko1 from '../../../4-svgs/world/europe/czechia/kraj-vysocina/kraj-vysocina.aggregated2.geojson.lod-5.svg';
-import kralovehradeckyKraj1 from '../../../4-svgs/world/europe/czechia/kralovehradecky-kraj/kralovehradecky-kraj.aggregated2.geojson.lod-5.svg';
-import libereckyKraj1 from '../../../4-svgs/world/europe/czechia/liberecky-kraj/liberecky-kraj.aggregated2.geojson.lod-5.svg';
-import moravskoslezskyKraj1 from '../../../4-svgs/world/europe/czechia/moravskoslezsky-kraj/moravskoslezsky-kraj.aggregated2.geojson.lod-5.svg';
-import olomouckyKraj1 from '../../../4-svgs/world/europe/czechia/olomoucky-kraj/olomoucky-kraj.aggregated2.geojson.lod-5.svg';
-import pardubickyKraj1 from '../../../4-svgs/world/europe/czechia/pardubicky-kraj/pardubicky-kraj.aggregated2.geojson.lod-5.svg';
-import plzenskyKraj1 from '../../../4-svgs/world/europe/czechia/plzensky-kraj/plzensky-kraj.aggregated2.geojson.lod-5.svg';
-import zlinskyKraj1 from '../../../4-svgs/world/europe/czechia/zlinsky-kraj/zlinsky-kraj.aggregated2.geojson.lod-5.svg';
-import okresBenesovStredniCechyCesko from '../../../4-svgs/world/europe/czechia/stredocesky-kraj/stredocesky-kraj.aggregated2.geojson.lod-5.svg';
-import usteckyKraj1 from '../../../4-svgs/world/europe/czechia/ustecky-kraj/ustecky-kraj.aggregated2.geojson.lod-5.svg';
+import hlavniMestoPrahaPrahaCesko from '../../../4-svgs/world/europe/czechia/czechia.aggregated3.geojson.lod-13.svg';
+import hlavniMestoPrahaPrahaCesko1 from '../../../4-svgs/world/europe/czechia/czechia.aggregated2.geojson.lod-13.svg';
+import untitled from '../../../4-svgs/world/europe/czechia/czechia.geojson.lod-13.svg';
+import hlavniMestoPrahaPrahaCesko2 from '../../../4-svgs/world/europe/czechia/hlavni-mesto-praha/hlavni-mesto-praha.geojson.lod-13.svg';
+import jihomoravskyKraj from '../../../4-svgs/world/europe/czechia/jihomoravsky-kraj/jihomoravsky-kraj.geojson.lod-13.svg';
+import jihoceskyKraj from '../../../4-svgs/world/europe/czechia/jihocesky-kraj/jihocesky-kraj.geojson.lod-13.svg';
+import karlovarskyKraj from '../../../4-svgs/world/europe/czechia/karlovarsky-kraj/karlovarsky-kraj.geojson.lod-13.svg';
+import krajVysocinaJihovychodCesko from '../../../4-svgs/world/europe/czechia/kraj-vysocina/kraj-vysocina.geojson.lod-13.svg';
+import kralovehradeckyKraj from '../../../4-svgs/world/europe/czechia/kralovehradecky-kraj/kralovehradecky-kraj.geojson.lod-13.svg';
+import libereckyKraj from '../../../4-svgs/world/europe/czechia/liberecky-kraj/liberecky-kraj.geojson.lod-13.svg';
+import moravskoslezskyKraj from '../../../4-svgs/world/europe/czechia/moravskoslezsky-kraj/moravskoslezsky-kraj.geojson.lod-13.svg';
+import olomouckyKraj from '../../../4-svgs/world/europe/czechia/olomoucky-kraj/olomoucky-kraj.geojson.lod-13.svg';
+import pardubickyKraj from '../../../4-svgs/world/europe/czechia/pardubicky-kraj/pardubicky-kraj.geojson.lod-13.svg';
+import plzenskyKraj from '../../../4-svgs/world/europe/czechia/plzensky-kraj/plzensky-kraj.geojson.lod-13.svg';
+import stredoceskyKraj from '../../../4-svgs/world/europe/czechia/stredocesky-kraj/stredocesky-kraj.geojson.lod-13.svg';
+import zlinskyKraj from '../../../4-svgs/world/europe/czechia/zlinsky-kraj/zlinsky-kraj.geojson.lod-13.svg';
+import usteckyKraj from '../../../4-svgs/world/europe/czechia/ustecky-kraj/ustecky-kraj.geojson.lod-13.svg';
+import jihomoravskyKraj1 from '../../../4-svgs/world/europe/czechia/jihomoravsky-kraj/jihomoravsky-kraj.aggregated2.geojson.lod-13.svg';
+import jihoceskyKraj1 from '../../../4-svgs/world/europe/czechia/jihocesky-kraj/jihocesky-kraj.aggregated2.geojson.lod-13.svg';
+import karlovarskyKraj1 from '../../../4-svgs/world/europe/czechia/karlovarsky-kraj/karlovarsky-kraj.aggregated2.geojson.lod-13.svg';
+import krajVysocinaJihovychodCesko1 from '../../../4-svgs/world/europe/czechia/kraj-vysocina/kraj-vysocina.aggregated2.geojson.lod-13.svg';
+import kralovehradeckyKraj1 from '../../../4-svgs/world/europe/czechia/kralovehradecky-kraj/kralovehradecky-kraj.aggregated2.geojson.lod-13.svg';
+import libereckyKraj1 from '../../../4-svgs/world/europe/czechia/liberecky-kraj/liberecky-kraj.aggregated2.geojson.lod-13.svg';
+import moravskoslezskyKraj1 from '../../../4-svgs/world/europe/czechia/moravskoslezsky-kraj/moravskoslezsky-kraj.aggregated2.geojson.lod-13.svg';
+import olomouckyKraj1 from '../../../4-svgs/world/europe/czechia/olomoucky-kraj/olomoucky-kraj.aggregated2.geojson.lod-13.svg';
+import pardubickyKraj1 from '../../../4-svgs/world/europe/czechia/pardubicky-kraj/pardubicky-kraj.aggregated2.geojson.lod-13.svg';
+import plzenskyKraj1 from '../../../4-svgs/world/europe/czechia/plzensky-kraj/plzensky-kraj.aggregated2.geojson.lod-13.svg';
+import zlinskyKraj1 from '../../../4-svgs/world/europe/czechia/zlinsky-kraj/zlinsky-kraj.aggregated2.geojson.lod-13.svg';
+import okresBenesovStredniCechyCesko from '../../../4-svgs/world/europe/czechia/stredocesky-kraj/stredocesky-kraj.aggregated2.geojson.lod-13.svg';
+import usteckyKraj1 from '../../../4-svgs/world/europe/czechia/ustecky-kraj/ustecky-kraj.aggregated2.geojson.lod-13.svg';
 
 // TODO: !!! Generator warning
 
@@ -66,7 +65,6 @@ declareModule(
                         items: [
                             { title: 'Hlavní město Praha, Praha, Česko', imageSrc: hlavniMestoPrahaPrahaCesko },
                             { title: 'Hlavní město Praha, Praha, Česko', imageSrc: hlavniMestoPrahaPrahaCesko1 },
-                            { title: 'Hlavní město Praha, Praha, Česko', imageSrc: hlavniMestoPrahaPrahaCesko2 },
                             { title: 'Untitled', imageSrc: untitled },
                         ],
                     },
@@ -79,7 +77,7 @@ declareModule(
                     {
                         title: '',
                         items: [
-                            { title: 'Hlavní město Praha, Praha, Česko', imageSrc: hlavniMestoPrahaPrahaCesko3 },
+                            { title: 'Hlavní město Praha, Praha, Česko', imageSrc: hlavniMestoPrahaPrahaCesko2 },
                             { title: 'Jihomoravský kraj', imageSrc: jihomoravskyKraj },
                             { title: 'Jihočeský kraj', imageSrc: jihoceskyKraj },
                             { title: 'Karlovarský kraj', imageSrc: karlovarskyKraj },
